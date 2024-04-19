920715e52e61b6bde7061334456a03c914a68a53
stav1236 <stavmaor1@gmail.com>
2024-04-19 14:56:57 +0300

    isLegalMove

--- main.js
+++ main.js
@@ -4,15 +4,30 @@ const EMPTY_CELL = 0;
 const PLAYER_1 = 1;
 const PLAYER_2 = 2;
 
-const createingNewBoard = (boardSize) => {
-    return Array.from({ length: boardSize }, () => Array(boardSize).fill(EMPTY_CELL));
+const createingNewBoard = () => {
+  return Array.from({ length: BOARD_SIZE }, () =>
+    Array(BOARD_SIZE).fill(EMPTY_CELL)
+  );
 };
 
 const printMatrix = (matrix) => {
-    for (let row of matrix) {
-        console.log(row.join(" "));
-    }
-}
+  for (let row of matrix) {
+    console.log(row.join(" "));
+  }
+};
 
-const BOARD = createingNewBoard(BOARD_SIZE);
+const BOARD = createingNewBoard();
 printMatrix(BOARD);
+
+const isEmptyCell = (board, i, j) => board[i][j] == EMPTY_CELL;
+
+const isOutOfTheBoard = (i, j) =>
+  i < 0 || j < 0 || i >= BOARD_SIZE || j >= BOARD_SIZE;
+
+const isLegalMove = (board, i, j) => {
+  return !isOutOfTheBoard(i, j) && isEmptyCell(board, i, j);
+};
+
+const Game = (board) => {};
+
+Game(BOARD);
